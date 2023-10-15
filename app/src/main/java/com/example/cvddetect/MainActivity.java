082e7c2963ed2c010795a27f5fb6cde0daee746b
Prabhati <prabhatibhatt12@gmail.com>
2023-10-15 18:07:38 +0530

    ui done

--- app/src/main/java/com/example/cvddetect/MainActivity.java
+++ app/src/main/java/com/example/cvddetect/MainActivity.java
@@ -3,9 +3,20 @@ package com.example.cvddetect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public void btn1(View view){
+        Log.i("btn1", "this is a message");
+    }
 
+    public void btn2(View view){
+        Log.i("btn2", "a message");
+    }
+    public void btn3(View view){
+        Log.i("btn3", "submit message");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
